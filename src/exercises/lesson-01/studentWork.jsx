cf115de7e3c9b3ef05d6696911430554d88c9a37
ShonTechDev <shonta.pierce@gmail.com>
2026-04-06 22:44:41 -0400

    exercise lesson-01 completed

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,26 @@
 
 export default function StudentWork() {
   //add variables here
+  const myName = 'Shonta';
+  const myAge = 39;
+  const myLocation = 'United States';
+
+  const myHobbies = ['reading', 'singing', 'web development'];
+
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>
+        Hello! I'm {myName}, I'm {myAge}, and I'm excited to be a student in
+        Code The Dream's React class. I'm motivated and happy to learn all I
+        can!
+      </p>
+      <h2>My Hobbies</h2>
+      <ul>
+        {myHobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
