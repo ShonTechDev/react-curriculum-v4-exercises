f02795049e1fd2fd387425bf9a90e797ab69c6fc
ShonTechDev <shonta.pierce@gmail.com>
2026-06-10 20:31:23 -0400

    Completed lesson 10 React Router exercises

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,18 @@
 import { Link, useLocation } from 'react-router-dom';
 
 export default function NotFound() {
-  const pathname = '';
+  const location = useLocation();
+  const pathname = location.pathname;
 
   return (
     <section>
       <h2>404: Not Found</h2>
+
+      <p>
+        No route found for: <code>{pathname}</code>
+      </p>
+
+      <Link to="/lessons/lesson-10">Go Home</Link>
     </section>
   );
 }
